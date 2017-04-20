f8e7452993f8d72df78eead3bbd54e6c59b37578
Neil James Cook <neil.james.cook@gmail.com>
2017-04-20 14:45:53 -0400

    Added Rectangle Selector function and Add button function to init

---
+++ __init__.py
@@ -0,0 +1,20 @@
+from . import Add_buttons
+from . import Rectangle_Selector
+
+
+__author__ = "Neil Cook"
+__email__ = 'neil.james.cook@gmail.com'
+__version__ = '0.1'
+__all__ = ['Add_buttons', 'Rectangle_Selector']
+
+# =============================================================================
+# Rectangle Functions
+# =============================================================================
+SelectRectangle = Rectangle_Selector.Select_Rectange
+
+# =============================================================================
+# Button Functions
+# =============================================================================
+AddButtons = Add_buttons.Add_Buttons
+
+
